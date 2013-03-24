3abbfb86a562ecfd058f13733fda3c73d2ae523c
shrimpboyho <shrimpboyhoftw@gmail.com>
2013-03-24 16:06:44 -0400

    THE QUIZ LOGIC WORKS COMPLETELY!!! TOOK YEARS TO DEVELOP BUT IT WORKS!!!

--- code.js
+++ code.js
@@ -1,7 +1,11 @@
-// Hide everything but the homepage div (the homepage)
+// Hide everything but the homepage div (the homepage) (also hide all the quiz questions) Set up global variables
 
 	$('.everythingButHomepage').hide();
+	$('.quizQuestions').hide();
+	var arrayLoopingVar = 0;
 
+	var correctScore = 0;
+	var wrongScore = 0;
 
 // Set up the homepage div and the menu div transition function
 
@@ -52,27 +56,73 @@
 
 	function setUpQuiz(){
 
-		$('#answerCheckerModal').modal('show');
 
-		/*...SET RANDOM NUMBER SEQUENCE HERE...*/
+		/*...SET RANDOM NUMBER SEQUENCE HERE AND LOG IT...*/
+		
+		$.get('http://www.random.org/sequences/?min=1&max=20&col=1&format=plain&rnd=new', function(data) {
+				  
+			mainstream(data.split("\n"));	
+			console.log(data.split("\n"));
+			  	
+		});
 
-			var randomSetCounter;
-			//var arrayOfRandom[14];
+	}
+	
+	function mainstream(numRanSequenceBank){
 
-			/*
-			for(randomSetCounter = 0; randomSetCounter < 15; randomSetCounter++){
+		if(numRanSequenceBank[arrayLoopingVar] == ""){
+			$("#quizModeDiv").append("<div><h2>Number Correct: " + correctScore + "</h2>" + "<h2>Number Wrong: " + wrongScore +"</h2></div>");
+		}
 
-				arrayOfRandom[randomSetCounter] = GENERATED_RANDOM_NUMBER;
+		// Show the current question
 
-			}
-			*/
+		$("#" + numRanSequenceBank[arrayLoopingVar]).show('slow');
 
-		/*...SET UP FIRST QUESTION DIV...*/
+		$(".answerChoices").unbind("click").click(function(){
+
+
+			// Check to see if the answer is right or wrong
+
+				if($(this).hasClass('correctAnswer')){
+
+					$('#answerCheckerModalTitle').text("Correct!");
+
+					// Make the popup show
+					$('#answerCheckerModal').modal('show');
+
+					// Increment score counter
+
+					correctScore++;
+					$("#" + numRanSequenceBank[arrayLoopingVar]).hide('slow');
+					arrayLoopingVar++;
+					mainstream(numRanSequenceBank);
+
+				}
+
+				if($(this).hasClass('wrongAnswer')){
+
+					
+					$('#answerCheckerModalTitle').text("Wrong!");
+
+					// Make the popup show
+					$('#answerCheckerModal').modal('show');
+
+					// Increment score counter
+
+					wrongScore++;
+					$("#" + numRanSequenceBank[arrayLoopingVar]).hide('slow');
+					arrayLoopingVar++;	
+					mainstream(numRanSequenceBank);
+
+				}
+
+		});
 
 		
 
-
 	}
+
+
 
 
 /*===================CHALLENGE MODE LOGIC======================*/
